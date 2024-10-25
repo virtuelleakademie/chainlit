9e36522d0f06180d00e96df293a86fdbc52952e9
gthomas2 <brudinie@gmail.com>
2024-10-25 21:40:08 +0100

    Display full screen wrapper if timer expired

--- frontend/src/components/organisms/chat/returnButton.tsx
+++ frontend/src/components/organisms/chat/returnButton.tsx
@@ -1,9 +1,24 @@
-import {useCallback, useEffect, useMemo, useState} from 'react';
+import styled from '@emotion/styled';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Box, Button } from '@mui/material';
 
 import { useAuth } from '../../../api/auth';
+
+const FullScreenWrapper = styled(Box)`
+  background-color: #ffffff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  z-index: 9999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const ReturnButton = () => {
   const { user } = useAuth();
@@ -17,10 +32,13 @@ export const ReturnButton = () => {
     const expiresIn = user?.metadata?.exp;
     let timeRemaining = expiresIn * 1000 - Date.now();
     timeRemaining = timeRemaining < 0 ? 0 : timeRemaining;
-    return Math.round(timeRemaining / 1000);
+    return timeRemaining === 0 ? 0 : Math.round(timeRemaining / 1000);
   }, [user]);
 
-  const [secondsRemaining, setSecondsRemaining] = useState(calcSecondsRemaining());
+  const secondsRemainingRef = useRef(calcSecondsRemaining());
+  const [secondsRemaining, setSecondsRemaining] = useState(
+    secondsRemainingRef.current
+  );
 
   const { t } = useTranslation();
 
@@ -30,26 +48,30 @@ export const ReturnButton = () => {
 
   // Set up an interval to update the countdown every second
   useEffect(() => {
-    if (secondsRemaining === null) {
+    if (secondsRemainingRef.current === null) {
       return;
     }
-    if (secondsRemaining <= 0) {
-      returnToSurvey();
+    if (Number(secondsRemainingRef.current) === 0) {
       return;
     }
     const interval = setInterval(() => {
-      if (secondsRemaining <= 0) {
+      console.log('!!!secondsRemaining', secondsRemainingRef.current);
+
+      if (Number(secondsRemainingRef.current) === 0) {
+        console.log('!!!CLEARING INTERVAL');
         clearInterval(interval); // Clear the interval if time is up
+        console.log('!!!RETURNING TO SURVEY');
         returnToSurvey();
         return;
       }
 
-      setSecondsRemaining(calcSecondsRemaining());
-    }, 1000); // Run every second (1000 ms)
+      secondsRemainingRef.current = calcSecondsRemaining();
+      setSecondsRemaining(secondsRemainingRef.current);
+    }, 500);
 
     // Clean up the interval on component unmount
     return () => clearInterval(interval);
-  }, []);
+  }, [calcSecondsRemaining]);
 
   const timerStr = useMemo(() => {
     if (!secondsRemaining) {
@@ -68,7 +90,15 @@ export const ReturnButton = () => {
     return null;
   }
 
-  return (
+  const expired = secondsRemaining !== null && secondsRemaining <= 0;
+
+  return expired ? (
+    <FullScreenWrapper>
+      <Button variant="contained" onClick={returnToSurvey}>
+        {t('components.organisms.chat.returnButton', { timer: timerStr })}
+      </Button>
+    </FullScreenWrapper>
+  ) : (
     <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
       <Button variant="contained" onClick={returnToSurvey}>
         {t('components.organisms.chat.returnButton', { timer: timerStr })}
